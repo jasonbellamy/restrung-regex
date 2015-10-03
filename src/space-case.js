5153b4cebc029364b591113934c4b97d35402644
Jason Bellamy <j@sonbellamy.com>
2015-10-03 13:17:26 -0400

    Refactor spaceCase to combine "clean" & "replace" into one regex

--- src/space-case.js
+++ src/space-case.js
@@ -1,8 +1,7 @@
 const spaceCase = (a) => {
   return a
-    .replace(/(_|-|\.)/g, ' ')
     .replace(/([A-Z])/g, ' $1')
-    .replace(/\s+/g, ' ')
+    .replace(/(_|-|\.|\s+)/g, ' ')
     .replace(/(^\s|\s$)/g, '');
 };
 
