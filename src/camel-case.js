11ddbe61996867d2c83957704b41322c471de818
Jason Bellamy <j@sonbellamy.com>
2015-10-05 22:26:19 -0400

    Fix the initial uncapitalize being too greedy

--- src/camel-case.js
+++ src/camel-case.js
@@ -1,7 +1,7 @@
 const camelCase = (a) => {
   return a
   .replace(/(^\s+|\s+$)/g, '')
-  .replace(/(^\w+)/, (match, a) => a.toLowerCase() )
+  .replace(/(^\w.)/, (match, a) => a.toLowerCase() )
   .replace(/([A-Z])/g, ' $1')
   .replace(/(-|_|\.)/g, ' ')
   .replace(/(\s[a-z]?)/g, (match, a) => a.toUpperCase() )
